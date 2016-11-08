6796428ac3fd3d3f863f0ee431999ce1a2255217
tuckyeah <h.rosebrock@gmail.com>
2016-11-08 12:02:14 -0500

    move to update rails-api-template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :category_contents, except: [:new, :edit]
-  resources :things, only: [:show, :edit, :destroy]
-  resources :categories, except: [:new, :edit] do
-    resources :things, only: [:index, :create]
-  end
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
