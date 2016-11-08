aaa1dcff079d54cc37d29c500dfb98ab64d75197
tuckyeah <h.rosebrock@gmail.com>
2016-11-08 12:00:14 -0500

    add category_contents to thing/category serializers

--- app/serializers/thing_serializer.rb
+++ app/serializers/thing_serializer.rb
@@ -1,8 +1,12 @@
 class ThingSerializer < ActiveModel::Serializer
-  attributes :id, :name, :categories
+  attributes :id, :name, :categories, :category_contents
   has_many :categories, through: :category_contents
 
   def categories
     object.categories.pluck(:id)
+  end
+
+  def category_contents
+    object.category_contents.pluck(:id)
   end
 end
